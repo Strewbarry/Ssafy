66ad2c8b77494dfb419b6279742fb5a959b66ea6
Strewbarry <chamsy39@gmail.com>
2023-03-03 22:27:24 +0900

    230303

---
+++ A형기출/활주로.cpp
@@ -0,0 +1,177 @@
+공유된 코드
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+#include<iostream>
+using namespace std;
+int N, X;
+int MAP[20][20];
+void input()
+{
+	cin >> N >> X;
+	for (int row = 0; row < N; row++)
+	{
+		for (int col = 0; col < N; col++)
+		{
+			cin >> MAP[row][col];
+		}
+	}
+}
+int validCheck(int arr[20]) {
+	int marked[20] = { 0 };
+	for (int i = 0; i < N - 1; i++) {
+		if (abs(arr[i + 1] - arr[i]) > 1)
+			return 0;
+		if (arr[i + 1] == arr[i]) continue;
+		if (arr[i] < arr[i + 1]) {
+			for (int j = i; j > i - X; j--)
+			{
+				if (j < 0) {
+					return 0;
+				}
+				if (arr[i] != arr[j]) {
+					return 0;
+				}
+				if (marked[j] == 1)
+					return 0;
+				marked[j] = 1;
+			}
+		}
+		else if (arr[i] > arr[i + 1]) {
+			for (int j = i + 1; j <= i + X; j++)
+			{
+				if (j >= N) return 0;
+				if (arr[i + 1] != arr[j]) {
+					return 0;
+				}
+				if (marked[j] == 1)
+					return 0;
+				marked[j] = 1;
+			}
+		}
+	}
+	return 1;
+}
+int main()
+{
+	//freopen_s(new FILE*, "text활주로.txt", "r", stdin); 
+	int T;
+	cin >> T;
+	for (int tc = 1; tc <= T; tc++)
+	{
+		input();
+		int cnt = 0;
+		for (int row = 0; row < N; row++) {
+			int temp[20];
+			for (int col = 0; col < N; col++) {
+				temp[col] = MAP[row][col];
+			}
+			if (validCheck(temp))
+			{
+				cnt++;
+			}
+		}
+		for (int col = 0; col < N; col++) {
+			int temp[20];
+			for (int row = 0; row < N; row++) {
+				temp[row] = MAP[row][col];
+			}
+			if (validCheck(temp))
+			{
+				cnt++;
+			}
+		}
+		cout << "#" << tc << " " << cnt << endl;
+	}
+	return 0;
+}
+
+
+
+© Color Scripter. All rights reserved.
